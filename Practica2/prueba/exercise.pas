9006e92322a2f1ab81ecc9166f6b1ba9114c9105
Daza yamil <dazayamil07@gmail.com>
2023-04-08 17:37:34 -0300

    examples finish

--- Practica2/prueba/exercise.pas
+++ Practica2/prueba/exercise.pas
@@ -1,4 +1,6 @@
 program practice;
+const 
+   valor_alto = 'zzz';
 type
    cadena = string[30];
    empleado = record
@@ -16,10 +18,10 @@ type
    maestro = file of empleado;
 
    //Process One.A
-   procedure readEmpleado(var e:empleado);
+   procedure writeEmpleado(var e:empleado);
    begin
       write('- Name: '); readln(e.name);
-      if(e.nombre <> '')then begin
+      if(e.name <> '')then begin
          write('- Adress: '); readln(e.adress);
          write('- Work hours: '); readln(e.workHours);
          writeln();
@@ -36,22 +38,77 @@ type
    begin
       write('Enter the name of the master file: '); readln(mae1Fisico);
       write('Enter detail file name: '); readln(det1Fisico);
+      assign(mae1, mae1Fisico);
+      assign(det1, det1Fisico);
       rewrite(mae1);
       rewrite(det1);
       writeln();
 
       writeln(' *** Master file load ***');
-      readEmpleado(e); //Process One.A
+      writeEmpleado(e); //Process One.A
       while(e.name <> '')do begin
          write(mae1, e);
-         readEmpleado(e);
+         writeEmpleado(e);
       end;
       writeln();
 
       writeln('*** Detail file load ***');
-      emp_diario.nombre:= 'CCC';
-      emp_diario.horasDeTrabajo:= 14;
+      empD.name:= 'facundo';
+      empD.workHours:= 14;
+      write(det1, empD);
+      close(mae1);
+      close(det1);
+   end;
 
+   //Process Two
+   procedure showMaster(var mae1: maestro);
+      procedure readEmpleado(var mae1: maestro; var e:empleado);
+      begin
+         if(not EOF(mae1))then
+            read(mae1, e)
+         else
+            e.name:= valor_alto;
+      end;
+
+      procedure showEmpleado(e:empleado);
+      begin
+         writeln('Name ->  ',e.name);
+         writeln('Adress ->  ',e.adress);
+         writeln('Work hours ->  ',e.workHours);
+      end;
+   var
+      e:empleado;
+   begin
+      reset(mae1);
+      readEmpleado(mae1, e);
+      while(e.name <> valor_alto)do begin
+         showEmpleado(e);
+         writeln(); //Salto de linea
+         readEmpleado(mae1, e);
+      end;
+      close(mae1);
+   end;
+
+   //Process Three
+   procedure updateMaster(var mae1: maestro; var det1:detalle);
+   var
+      e:empleado;
+      empD: emp_diario;
+   begin
+      reset(mae1);
+      reset(det1);
+      while(not EOF(det1))do begin
+         read(mae1, e);
+         read(det1, empD);
+         while(e.name <> empD.name)do 
+            read(mae1, e);
+         
+         e.workHours:= e.workHours + empD.workHours;
+         seek(mae1, filepos(mae1) - 1);
+         write(mae1, e);
+      end;
+      close(mae1);
+      close(det1);
    end;
 
 var
@@ -60,6 +117,9 @@ var
 begin
    loadFiles(mae1, det1); //Process One
    showMaster(mae1); //Process Two *
+   
+   write('Press enter to continue_ '); readln();
+
    updateMaster(mae1, det1); //Process Three
    showMaster(mae1); // Two *
    write('Press enter to finalize_'); readln();
